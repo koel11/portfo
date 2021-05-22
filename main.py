a0f83c4d5caf017de23fac6eba3190050ae87621
joel <joelinfinity@gmail.com>
2021-05-22 22:07:31 +0800

    added main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+# @app.route('/about.html')
+# def about():
+#     return render_template('about.html')
+#
+# @app.route('/work.html')
+# def work():
+#     return render_template('work.html')
+#
+# @app.route('/works.html')
+# def works():
+#     return render_template('works.html')
+#
+# @app.route('/components.html')
+# def components():
+#     return render_template('components.html')
+#
+# @app.route('/contact.html')
+# def contact():
+#     return render_template('contact.html')
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        # quote char - text qualifier
+        # csv.QUOTE_MINIMAL telling to dont write quotes
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'not saved to dabase'
+    else:
+        return 'something went weong'
+
